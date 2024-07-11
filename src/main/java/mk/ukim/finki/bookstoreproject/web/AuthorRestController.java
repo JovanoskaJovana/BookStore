ca42375683ac4c2ef0c89ce9dcb71d540e4782f1
Jovana <jovana.jovanoska003@gmail.com>
2024-07-11 15:13:04 +0200

    Added AuthorRestController to handle author-related API requests and Added security configuration

---
+++ src/main/java/mk/ukim/finki/bookstoreproject/web/AuthorRestController.java
@@ -0,0 +1,51 @@
+package mk.ukim.finki.bookstoreproject.web;
+
+import mk.ukim.finki.bookstoreproject.model.Author;
+import mk.ukim.finki.bookstoreproject.model.dto.AuthorDto;
+import mk.ukim.finki.bookstoreproject.service.AuthorService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = {"http://localhost:3000"})
+public class AuthorRestController {
+
+    private final AuthorService authorService;
+
+    public AuthorRestController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @GetMapping("/authors")
+    public List<Author> findAll(){
+        return authorService.findAll();
+    }
+
+    @PostMapping("/authors/addAuthor")
+    public ResponseEntity<Author> saveAuthor(@RequestBody AuthorDto authorDto) {
+        return authorService.save(authorDto)
+                .map(author -> ResponseEntity.ok().body(author))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/authors/editAuthor/{id}")
+    public ResponseEntity<Author> editAuthor (@PathVariable Long id,
+                                              @RequestBody AuthorDto authorDto) {
+        return authorService.edit(id, authorDto)
+                .map(product -> ResponseEntity.ok().body(product))
+                .orElseGet(() -> ResponseEntity.badRequest().build());
+
+    }
+
+    @DeleteMapping("/authors/deleteAuthor/{id}")
+    public ResponseEntity<Author> deleteAuthorById (@PathVariable Long id) {
+        authorService.deleteById(id);
+        if (authorService.findById(id).isEmpty()) return ResponseEntity.ok().build();
+        return ResponseEntity.badRequest().build();
+    }
+
+
+}
--- src/main/java/mk/ukim/finki/bookstoreproject/web/BookRestController.java
+++ src/main/java/mk/ukim/finki/bookstoreproject/web/BookRestController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = {"http://localhost:3000"})
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001"})
 public class BookRestController {
 
     private final BookService bookService;
